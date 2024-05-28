756614d98c795f6ca6060e6a16a42f5eb2bf391a
AlesciDavide <davi.alesci@email.com>
2024-05-28 12:41:34 +0200

    aggiunta immagine

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-            message: 'Hello Vue!!'
+            message: 'Hello Vue!!',
+            urlImg: 'https://www.nerdface.it/wp-content/uploads/2023/01/la-gigantesca-ironia-del-bassissimo-gigi-la-trottola.jpg'
         }
     }
 }).mount('#test')
