c41222b6af1780c85a8227051305e0da1fafb0ba
AlesciDavide <davi.alesci@email.com>
2024-05-28 13:05:06 +0200

    Bonus Special

--- js/script.js
+++ js/script.js
@@ -5,7 +5,15 @@ createApp({
     data(){
         return{
             message: 'Hello Vue!!',
-            urlImg: 'https://www.nerdface.it/wp-content/uploads/2023/01/la-gigantesca-ironia-del-bassissimo-gigi-la-trottola.jpg'
+            urlImg: 'https://www.nerdface.it/wp-content/uploads/2023/01/la-gigantesca-ironia-del-bassissimo-gigi-la-trottola.jpg',
+            classes: ''
+        }
+    },
+    methods:{
+        attivaClass: function(){
+            let imgEl = document.querySelector('#imgUserClass');
+            imgEl.classList.toggle('filtroImmagine');
+
         }
     }
 }).mount('#test')
