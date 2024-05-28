7338904b78719d80200d4c8498b449f96dbe1a54
AlesciDavide <davi.alesci@email.com>
2024-05-28 13:32:53 +0200

    modifica aggiunta classe nel data

--- js/script.js
+++ js/script.js
@@ -6,13 +6,12 @@ createApp({
         return{
             message: 'Hello Vue!!',
             urlImg: 'https://www.nerdface.it/wp-content/uploads/2023/01/la-gigantesca-ironia-del-bassissimo-gigi-la-trottola.jpg',
-            classes: ''
+            sceltaClasse: '',
         }
     },
     methods:{
         attivaClass: function(){
-            let imgEl = document.querySelector('#imgUserClass');
-            imgEl.classList.toggle('filtroImmagine');
+            this.sceltaClasse = this.sceltaClasse == 'filtroImmagine' ? '': 'filtroImmagine';
 
         }
     }
